512970f93c31ef328b736626b4d4b047b16069c7
xavier <xlozano@myseneca.ca>
2022-08-14 09:44:35 -0400

    Used map instead of array, added date stamp of all outgoing messages, added an emmit for the tempUsername assigned on connection

--- server.js
+++ server.js
@@ -1,64 +1,67 @@
 const express = require('express');
 const app = express();
-
 const HTTP_PORT = process.env.PORT || 8080;
+
+//Keep track of date and time
 
 //Setup socket.io
 let http = require('http').Server(app);
 const io = require('socket.io')(http, {
     cors: {}
 });
+//Add a map of all clients
+let clientMap = new Map();
 
-//Store websocket connections
-let socketUsers = [];
+//messageGivenClient
+/*This function receives a socket and a message to be sent to an individual cient */
+let messageClient = function (clientSocket, clientMessageObject) {
+    io.to(clientSocket.id).emit('chat message', clientMessageObject);
+}
 
 io.on('connection', function (socket) {
+    //Assign temp username and add to clientMap
+    let tempUsername = "User-" + Math.floor(Math.random() * (100000 - 1 + 1)) + 1;
+    clientMap.set(socket.id, { socket: socket, userName: tempUsername });
 
-    //Assign temp username and push object into array
-    let tempUserName = "User-" + Math.floor(Math.random() * (100000 - 1 + 1)) + 1;
-    socketUsers.push({ socket: socket, userName: tempUserName });
-
+    //send recently connected user their temp username
+    io.to(socket.id).emit('tempUsername', { tempUsername: tempUsername })
     //Let clients know number of users online
-    for (let i = 0; i < socketUsers.length; i++) {
-        if (socketUsers[i].socket.id != socket.id)
-            io.to(socketUsers[i].socket.id).emit('chat message', 'New user has connected!');
-        else {
-            if (socketUsers.length < 2)
-                io.to(socketUsers[i].socket.id).emit('chat message', 'Welcome! You are currently the only person here');
-            else if (socketUsers.length == 2)
-                io.to(socketUsers[i].socket.id).emit('chat message', 'Welcome! There is currently one other user connected');
+    clientMap.forEach((objectReference, key) => {
+        let socketItselt = objectReference.socket;
+        let socketId = key;
+        if (socketId === socket.id) {
+            let message;
+            if (clientMap.size < 2)
+                message = 'Welcome! You are currently the only person here'
+            else if (clientMap.size === 2)
+                message = 'Welcome! There is 1 more user connected';
             else
-                io.to(socketUsers[i].socket.id).emit('chat message', `Welcome! there  are ${socketUsers.length - 1} other users connected`);
+                message = `Welcome! There are ${clientMap.size - 1} other users connected`;
+            messageClient(socketItselt, { message: message });
         }
-    }
+        else
+            messageClient(socketItselt, { message: `A new user has connected` });
+    })
 
     //Let clients know which user disconnected and remove him from the array
     socket.on('disconnect', function () {
-        for (let i = 0; i < socketUsers.length; i++) {
-            if (socketUsers[i].socket.connected === false) {
-                io.emit('chat message', `${socketUsers[i].userName} has disconnected`);
-                socketUsers.splice(i, 1);
-                break;
-            }
-        }
+        io.emit('chat message', { message: `${clientMap.get(socket.id).userName} has disconnected!` });
+        clientMap.delete(socket.id);
     })
 
     //Check for incomming chat messages and re-emit them
     socket.on('chat message', function (user) {
+        //Add timestamp
+        let todaysDate = new Date(Date.now());
         if (!user.nickName)
-            user.nickName = tempUserName;
-        io.emit('chat message', user.nickName + ": " + user.message);
+            user.nickName = clientMap.get(socket.id).userName;
+        io.emit('chat message', { serverDate: todaysDate, nickName: user.nickName, message: `${user.nickName}: ${user.message}` });
     })
 
     //If client updated their nickname/username, we update our array
     socket.on('usernameUpdated', function (user) {
-        for (let i = 0; i < socketUsers.length; i++) {
-            if (socketUsers[i].userName === tempUserName) {
-                socketUsers[i].userName = user.nickName;
-                break;
-            }
-        }
-        io.emit('chat message', `"${tempUserName}" is now "${user.nickName}"`);
+        io.emit('chat message', { message: `${clientMap.get(socket.id).userName} is now ${user.nickName}` })
+        clientMap.set(socket.id, { socket: socket, userName: user.nickName });
     })
 })
 
